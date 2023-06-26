80611ca4d23706f268bac253b75f91025e357457
Wzchen <wiizard-chen@outlook.com>
2023-06-26 22:55:18 +0800

    feat: add solidjs

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import type { Component } from "solid-js";
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+const App: Component = () => {
+  return <div class={styles.App}></div>;
+};
+
+export default App;
